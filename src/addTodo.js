003bd7174545dfcdc12d55b8ca6e36d0d639dc38
Eapen Mani <eapenmani@gmail.com>
2024-04-15 21:38:30 +1000

    Added the lambda function for fect and create todos

--- src/addTodo.js
+++ src/addTodo.js
@@ -1,18 +1,32 @@
-const hello = async (event) => {
+const {v4} = require("uuid")
+const AWS = require("aws-sdk")
+
+const addTodo = async (event) => {
+  const { todo } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id=v4();
+  const dynamodb = new AWS.DynamoDB.DocumentClient()
+  console.log("This is an id",id)
+
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    complete: false
+  }
+  
+  dynamodb.put({
+    TableName: "TodoTable",
+    Item: newTodo
+  })
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(newTodo),
   };
 };
 
 
 module.exports ={
-  handler: hello
+  handler: addTodo
 }
